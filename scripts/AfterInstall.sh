00ff95681c7322495e10ae07a3feab1c916fdafa
Mathijs Kingma <mathijskingma@gmail.com>
2016-10-04 16:12:41 +0200

    Update

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd /webapps/rails
-RAILS_ENV=production bundle install --path vendor/bundle
-RAILS_ENV=production bundle exec rake assets:clobber
-RAILS_ENV=production bundle exec rake assets:precompile
+# cd /webapps/rails
+# RAILS_ENV=production bundle install --path vendor/bundle
+# RAILS_ENV=production bundle exec rake assets:clobber
+# RAILS_ENV=production bundle exec rake assets:precompile
+#
+#
+# rbenv sudo passenger start -p80
+# # Restarten van applicatie
+# passenger-config restart-app /webapps/rails
